7587de2d0df2d25a43417d663fd61f98f422a852
aray1430 <arunimaray@innotical.com>
2018-09-06 16:05:11 +0530

    change

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ Eos = require('eosjs')
 config = {
   chainId: "038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca", // 32 byte (64 char) hex string
   keyProvider: ['5JMsRFvmd7NQUniCym2Kd7HhuJGh2VhHesT1K3TLCTWAuhutpRf'], // WIF string or array of keys..
-  httpEndpoint: 'http://193.93.219.219:8888',
+  httpEndpoint: 'https://jungle.eosio.cr',
   expireInSeconds: 60,
   broadcast: true,
   verbose: false, // API activity
