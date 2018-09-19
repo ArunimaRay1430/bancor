e3c2f706baa745036c2c2001a26539abaca8a1fc
aray1430 <arunimaray@innotical.com>
2018-09-19 13:14:19 +0530

    change

--- server.js
+++ server.js
@@ -11,10 +11,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
-});
+  });
 
 Eos = require('eosjs')
 
